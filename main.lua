a801c2f9863bd8be5f5ae57d773b69de107e88ef
bytepenguin0 <totreadrian@gmail.com>
2024-11-05 18:01:26 +0100

    dragify fix

--- main.lua
+++ main.lua
@@ -176,8 +176,6 @@ local InitializeToggles = function()
                     Callback()
                 end
             end
-        
-            print("Working!")
         end)
     end)
 end
@@ -189,8 +187,49 @@ local InitializeDragify = function()
         return
     end
 
-    local Helpers = loadstring(game:HttpGet("https://raw.githubusercontent.com/certified-retart/helpers/refs/heads/main/utility.lua"))()
-    return Helpers.draggable(Window, 30)
+    local dragToggle
+    local dragInput
+    local dragSpeed
+    local dragStart
+    local dragPos
+    local startPos
+    
+    function dragify(Frame)
+        dragToggle = nil
+        dragSpeed = 0.50
+        dragInput = nil
+        dragStart = nil
+        dragPos = nil
+        local function updateInput(input)
+            local Delta = input.Position - dragStart
+            local Position = UDim2.new(startPos.X.Scale, startPos.X.Offset + Delta.X, startPos.Y.Scale, startPos.Y.Offset + Delta.Y)
+            game:GetService("TweenService"):Create(Frame, TweenInfo.new(0.30), {Position = Position}):Play()
+        end
+        Frame.InputBegan:Connect(function(input)
+            if (input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch) and game:GetService("UserInputService"):GetFocusedTextBox() == nil then
+                dragToggle = true
+                dragStart = input.Position
+                startPos = Frame.Position
+                input.Changed:Connect(function()
+                    if input.UserInputState == Enum.UserInputState.End then
+                        dragToggle = false
+                    end
+                end)
+            end
+        end)
+        Frame.InputChanged:Connect(function(input)
+            if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
+                dragInput = input
+            end
+        end)
+        game:GetService("UserInputService").InputChanged:Connect(function(input)
+            if input == dragInput and dragToggle then
+                updateInput(input)
+            end
+        end)
+    end
+    
+    dragify(Window)
 end
 
 local InitializeButtons = function()
